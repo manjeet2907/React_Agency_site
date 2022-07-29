b8f73a4752286b8d86b34df2e38255432dc519e6
Manjeet Saini <manjeet29saini@gmail.com>
2022-07-29 07:20:47 +0530

    updating changes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
-import React from 'react';
+import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
-import Contact from './Pages/Contact';
-import Home from './Pages/Home';
-import Service from './Pages/Service';
-import About from './Pages/About';
-import './Styles/App.css';
-import { Switch, Route, Redirect } from 'react-router-dom';
-import Navbar from './Pages/Components/Navbar';
-import Footer from './Pages/Components/Footer';
-
-
+import Contact from "./Pages/Contact";
+import Home from "./Pages/Home";
+import Service from "./Pages/Service";
+import About from "./Pages/About";
+import "./Styles/App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./Pages/Components/Navbar";
+import Footer from "./Pages/Components/Footer";
 
 function App() {
-    return (
-        <div className="app">
-        <Navbar/>
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route path="/about" component={About} />
-                <Route path="/service" component={Service} />
-                <Route path="/contact" component={Contact} />
-            </Switch>
-            <Footer/>
-        </div>
-    )
+  return (
+    <div className='app'>
+      <Navbar />
+      <Routes>
+        <Route end path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/service' element={<Service />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
